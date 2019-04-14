986ec7aa1a86560d76ded35bc74dd763aa1c3cfa
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-14 18:11:49 +0200

    removed start button

--- client/src/components/StartSequence.jsx
+++ client/src/components/StartSequence.jsx
@@ -6,9 +6,7 @@ const StartSequence = ({ store }) => {
   if (store.ready && !store.set && !store.go && !store.timerIsOn) {
     return (
       <div className={styles.wrapper}>
-        <button className={styles.red} onClick={store.startSequence}>
-          Ready
-        </button>
+        <button className={styles.red}>Ready</button>
         <button className={styles.base}>Set</button>
         <button className={styles.base}>GO</button>
       </div>
@@ -16,9 +14,7 @@ const StartSequence = ({ store }) => {
   } else if (store.ready && store.set && !store.go && !store.timerIsOn) {
     return (
       <div className={styles.wrapper}>
-        <button className={styles.red} onClick={store.startSequence}>
-          Ready
-        </button>
+        <button className={styles.red}>Ready</button>
         <button className={styles.red}>Set</button>
         <button className={styles.base}>GO</button>
       </div>
@@ -26,9 +22,7 @@ const StartSequence = ({ store }) => {
   } else if (store.ready && store.set && store.go && store.timerIsOn) {
     return (
       <div className={styles.wrapper}>
-        <button className={styles.green} onClick={store.startSequence}>
-          Ready
-        </button>
+        <button className={styles.green}>Ready</button>
         <button className={styles.green}>Set</button>
         <button className={styles.green}>GO</button>
       </div>
@@ -36,9 +30,7 @@ const StartSequence = ({ store }) => {
   } else {
     return (
       <div className={styles.wrapper}>
-        <button className={styles.ready} onClick={store.startSequence}>
-          Ready
-        </button>
+        <button className={styles.base}>Ready</button>
         <button className={styles.base}>Set</button>
         <button className={styles.base}>GO</button>
       </div>
