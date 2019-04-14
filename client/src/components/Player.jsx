3b0579f8050b4dba9b8fffd6695cba53c64e9db8
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-14 18:08:30 +0200

    ready button toegevoegd

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,16 +1,43 @@
 import React from "react";
 import styles from "./Player.module.css";
+import { inject, observer } from "mobx-react";
 
-const Player = ({ number, button }) => {
-  return (
-    <div className={styles.wrapper}>
-      <label>
-        <h2 className={styles.label}>Player {number}</h2>
-        <input className={styles.nameInput} type="text" />
-      </label>
-      <p className={styles.copy}>Press {button}</p>
-    </div>
-  );
+const Player = ({ store, number, button, setNameFunction, ready }) => {
+  const playerInput = React.createRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setNameFunction(playerInput.current.value);
+    store.checkForStartGame();
+  };
+
+  if (ready) {
+    return (
+      <form className={styles.wrapper} onSubmit={handleSubmit}>
+        <div>
+          <h2 className={styles.label}>Player {number}</h2>
+          <input className={styles.nameInput} type="text" ref={playerInput} />
+        </div>
+        <div className={styles.subtextWrapper}>
+          <p className={styles.copy}>Press {button}</p>
+          <input type="submit" value="ready" className={styles.buttonPressed} />
+        </div>
+      </form>
+    );
+  } else {
+    return (
+      <form className={styles.wrapper} onSubmit={handleSubmit}>
+        <div>
+          <h2 className={styles.label}>Player {number}</h2>
+          <input className={styles.nameInput} type="text" ref={playerInput} />
+        </div>
+        <div className={styles.subtextWrapper}>
+          <p className={styles.copy}>Press {button}</p>
+          <input type="submit" value="ready" className={styles.button} />
+        </div>
+      </form>
+    );
+  }
 };
 
-export default Player;
+export default inject("store")(observer(Player));
