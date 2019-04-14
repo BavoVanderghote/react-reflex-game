ff26c1ecb19d862a5b50b6c1db0ddd4a96aed04a
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-14 18:16:46 +0200

    game start via players ready, kleine bug fixes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,16 @@
 import { decorate, configure, observable, action } from "mobx";
+// import Score from "../models/Score";
+import Api from "../api";
 
 configure({ enforceActions: "observed" });
 
 class Store {
   start;
   interval;
+  p1Name = `player 1`;
+  p2Name = `player 2`;
+  p1Ready = false;
+  p2Ready = false;
   p1 = 0;
   p2 = 0;
   p1Interval;
@@ -13,12 +19,31 @@ class Store {
   p2Finished = false;
 
   timerIsOn = false;
+  gameIsFinished = false;
+  winner = {};
+  loser = {};
 
   ready = false;
   set = false;
   go = false;
 
+  constructor() {
+    this.api = new Api("books");
+  }
+
+  //GAME
+
+  checkForStartGame = () => {
+    if (this.p1Ready && this.p2Ready) {
+      this.startSequence();
+    }
+  };
+
   startSequence = () => {
+    this.gameIsFinished = false;
+    this.p1Finished = false;
+    this.p2Finished = false;
+
     this.ready = false;
     this.set = false;
     this.go = false;
@@ -29,6 +54,7 @@ class Store {
     // );
 
     console.log(`ready`);
+
     this.ready = true;
     setTimeout(this.setSet, 1000);
     setTimeout(this.startTimer, Math.random() * (5000 - 2000) + 2000);
@@ -40,6 +66,8 @@ class Store {
   };
 
   startTimer = () => {
+    console.log(`start game`);
+
     if (!this.timerIsOn) {
       this.go = true;
       this.timerIsOn = true;
@@ -52,45 +80,105 @@ class Store {
   };
 
   setP1 = () => {
-    console.log(this.p1);
+    // console.log(this.p1);
 
     this.p1 = (Date.now() - this.start) / 1000;
   };
 
+  setP1Name = name => {
+    console.log(`player 1 ready`);
+    this.p1Name = name;
+    this.p1Ready = true;
+  };
+
+  setP2Name = name => {
+    console.log(`player 2 ready`);
+    this.p2Name = name;
+    this.p2Ready = true;
+  };
+
   setP2 = () => {
-    console.log(this.p2);
+    // console.log(this.p2);
 
     this.p2 = (Date.now() - this.start) / 1000;
   };
 
   endTimer = (key, player) => {
-    console.log(key);
-    if (player === 1) {
+    if (player === 1 && this.timerIsOn) {
       clearInterval(this.p1Interval);
       this.p1Finished = true;
-      if ((Date.now() - this.start) / 1000) {
-        this.p1 = (Date.now() - this.start) / 1000;
+
+      if (!this.p2Finished) {
+        this.winner.name = this.p1Name;
+        this.winner.time = this.p1;
       } else {
-        this.p1 = 0;
+        this.loser.name = this.p1Name;
+        this.loser.time = this.p1;
       }
-    } else if (player === 2) {
+    } else if (player === 2 && this.timerIsOn) {
       clearInterval(this.p2Interval);
       this.p2Finished = true;
-      if ((Date.now() - this.start) / 1000) {
-        this.p2 = (Date.now() - this.start) / 1000;
+
+      if (!this.p1Finished) {
+        this.winner.name = this.p2Name;
+        this.winner.time = this.p2;
       } else {
-        this.p2 = 0;
+        this.loser.name = this.p2Name;
+        this.loser.time = this.p2;
       }
+    } else {
+      console.log(`player ${player} pressed too early`);
+      //pressed too early code
     }
-    if (this.p1Finished && this.p2Finished) {
+    if (this.p1Finished && this.p2Finished && this.timerIsOn) {
       this.timerIsOn = false;
+      this.gameIsFinished = true;
+      this.p1Ready = false;
+      this.p2Ready = false;
+
+      console.log(`game finisched? ${this.gameIsFinished}`);
+      console.log(
+        `[winner] ${this.winner.name} took ${this.winner.time} seconds`
+      );
+      console.log(`[loser] ${this.loser.name} took ${this.loser.time} seconds`);
+      // this.addScore({ winner: this.winner, loser: this.loser });
     }
   };
+
+  //APi
+
+  // addScore = data => {
+  //   console.log(`addScore`);
+
+  //   const newScore = new Score({
+  //     winner: data.winner.name,
+  //     loser: data.loser.name,
+  //     winnerTime: data.winner.time,
+  //     loserTime: data.loser.time
+  //   });
+  //   console.log(`toe te voegen score ${newScore}`);
+  // newScore.updateFromServer(data);
+
+  // this.scores.push(newScore);
+  // this.api
+  //   .create(newScore)
+  //   .then(scoreValues => newScore.updateFromServer(scoreValues));
+  // };
+
+  // _addBook = values => {
+  //   const score = new Score(this.rootStore);
+  //   score.updateFromServer(values);
+  //   runInAction(() => this.books.push(score));
+  // };
 }
 
 decorate(Store, {
   start: observable,
   interval: observable,
+  p1Name: observable,
+  p2Name: observable,
+  p1Ready: observable,
+  p2Ready: observable,
   p1: observable,
   p2: observable,
   p1Interval: observable,
@@ -99,6 +187,9 @@ decorate(Store, {
   p2Finished: observable,
   timer: observable,
   timerIsOn: observable,
+  gameIsFinished: observable,
+  winner: observable,
+  loser: observable,
   ready: observable,
   set: observable,
   go: observable,
@@ -108,7 +199,9 @@ decorate(Store, {
   setP2: action,
   startSequence: action,
   setSet: action,
-  setGo: action
+  setGo: action,
+  setP1Name: action,
+  setP2Name: action
 });
 
 const store = new Store();
