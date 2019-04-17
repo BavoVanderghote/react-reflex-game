f603fd51f9d3cc2005accf5894134310e7d93e7d
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-17 17:35:31 +0200

    correct server connection

--- server/app/routes/score.routes.js
+++ server/app/routes/score.routes.js
@@ -1,9 +1,9 @@
 module.exports = app => {
   // const {checkToken} = require('../middleware/');
-  const controller = require("../controllers/score.controller.js");
-  app.post("/score", controller.create);
-  app.get("/score", controller.findAll);
-  app.get("/score/:scoreId", controller.findOne);
-  app.put("/score/:scoreId", controller.update);
-  app.delete("/score/:scoreId", controller.delete);
+  const controller = require('../controllers/score.controller.js');
+  app.post('/score', controller.create);
+  app.get('/score', controller.findAll);
+  app.get('/score/:scoreId', controller.findOne);
+  app.put('/score/:scoreId', controller.update);
+  app.delete('/score/:scoreId', controller.delete);
 };
