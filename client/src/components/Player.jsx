47bcd7b7d973131cd1c6e9f6bede2ddb9f29f8e7
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-18 22:15:05 +0200

    nieuw spel starten vanuit leaderboard

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Player.module.css";
 import { inject, observer } from "mobx-react";
 
-const Player = ({ store, number, button, setNameFunction, ready }) => {
+const Player = ({ store, number, button, setNameFunction, ready, name }) => {
   const playerInput = React.createRef();
 
   const handleSubmit = e => {
@@ -16,7 +16,12 @@ const Player = ({ store, number, button, setNameFunction, ready }) => {
       <form className={styles.wrapper} onSubmit={handleSubmit}>
         <div>
           <h2 className={styles.label}>Player {number}</h2>
-          <input className={styles.nameInput} type="text" ref={playerInput} />
+          <input
+            className={styles.nameInput}
+            type="text"
+            ref={playerInput}
+            defaultValue={name}
+          />
         </div>
         <div className={styles.subtextWrapper}>
           <p className={styles.copy}>Press {button}</p>
@@ -29,7 +34,12 @@ const Player = ({ store, number, button, setNameFunction, ready }) => {
       <form className={styles.wrapper} onSubmit={handleSubmit}>
         <div>
           <h2 className={styles.label}>Player {number}</h2>
-          <input className={styles.nameInput} type="text" ref={playerInput} />
+          <input
+            className={styles.nameInput}
+            type="text"
+            ref={playerInput}
+            defaultValue={name}
+          />
         </div>
         <div className={styles.subtextWrapper}>
           <p className={styles.copy}>Press {button}</p>
