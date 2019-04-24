1cef1f49e9181dbf959ad127f1acbada451b58ae
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-24 22:58:42 +0200

    serverside authenticatie implementatie

--- server/app/routes/score.routes.js
+++ server/app/routes/score.routes.js
@@ -1,9 +1,9 @@
 module.exports = app => {
-  // const {checkToken} = require('../middleware/');
+  const {checkToken} = require('../middleware/');
   const controller = require('../controllers/score.controller.js');
-  app.post('/score', controller.create);
-  app.get('/score', controller.findAll);
-  app.get('/score/:scoreId', controller.findOne);
-  app.put('/score/:scoreId', controller.update);
-  app.delete('/score/:scoreId', controller.delete);
+  app.post('/score', checkToken, controller.create);
+  app.get('/score', checkToken, controller.findAll);
+  app.get('/score/:scoreId', checkToken, controller.findOne);
+  app.put('/score/:scoreId', checkToken, controller.update);
+  app.delete('/score/:scoreId', checkToken, controller.delete);
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 mongoose
   .connect(process.env.DB_URL, {
@@ -11,9 +11,9 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true
   })
-  .then(() => console.log("db connected"))
+  .then(() => console.log('db connected'))
   .catch(e => {
-    console.log("Error, exiting", e);
+    console.log('Error, exiting', e);
     process.exit();
   });
 
@@ -21,15 +21,15 @@ const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "up and running" });
+app.get('/', (req, res) => {
+  res.json({message: 'up and running'});
 });
 
-// require('./app/routes/auth.routes.js')(app);
-require("./app/routes/score.routes.js")(app);
+require('./app/routes/auth.routes.js')(app);
+require('./app/routes/score.routes.js')(app);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server luistert op poort ${process.env.PORT}`);
