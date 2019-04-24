18da2519c81505b3fa4d5c405fab2b07733f6195
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-24 23:09:22 +0200

    alle stores meegeven

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,12 @@ import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
-import store from "./store/index";
+import store from "./store";
 import { Provider } from "mobx-react";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider {...store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
