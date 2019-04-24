a07df6062c90fc4ae78a93b8a00852d039e4ec54
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-24 23:08:14 +0200

    routes met authenticatie [NOT WORKING]

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,30 +1,61 @@
-import React, { Component } from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import { ROUTES } from "../constants/";
 import styles from "./App.module.css";
+import { inject } from "mobx-react";
+import Login from "../components/auth/Login";
+import Register from "../components/auth/Register";
 
 import Game from "./Game";
 import leaderboard from "./Leaderboard";
 
-class App extends Component {
-  render() {
-    return (
-      // <main className={styles.layout}>
-      <>
-        <header>
-          <h1 className={styles.title}>
-            React<span className={styles.titleInset}>ion</span> game
-          </h1>
-        </header>
-        <main className={styles.layout}>
-          <Switch>
-            <Route path={ROUTES.game} exact strict component={Game} />
-            <Route path={ROUTES.leaderboard} component={leaderboard} />
-          </Switch>
-        </main>
-      </>
-    );
-  }
-}
+const App = ({ uiStore }) => {
+  return (
+    // <main className={styles.layout}>
+    <>
+      <header>
+        <h1 className={styles.title}>
+          React<span className={styles.titleInset}>ion</span> game
+        </h1>
+      </header>
+      <main className={styles.layout}>
+        <Switch>
+          <Route path={ROUTES.game} exact strict component={Game} />
+          <Route path={ROUTES.leaderboard} component={leaderboard} />
+          <Route path={ROUTES.login} component={Login} />
+          <Route path={ROUTES.register} component={Register} />
+          <Route
+            path={ROUTES.landing}
+            exact
+            strict
+            render={() => (
+              <div className={styles.container}>
+                <p className={styles.label}>Welcome, human.</p>
+                {uiStore.authUser ? (
+                  <Link className={styles.link} to={ROUTES.game}>
+                    Play
+                  </Link>
+                ) : (
+                  <ul className={styles.container}>
+                    <li>
+                      <Link className={styles.link} to={ROUTES.login}>
+                        Log in
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className={styles.link} to={ROUTES.register}>
+                        Register
+                      </Link>
+                    </li>
+                  </ul>
+                )}
+              </div>
+            )}
+          />
+        </Switch>
+      </main>
+    </>
+  );
+};
 
-export default withRouter(App);
+export default inject("uiStore")(App);
