5f7ade3aeab0ea03a8c0aedaeb8bedc8afadbdaf
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-25 11:30:19 +0200

    game of login tonen bij ingelogd of niet

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ROUTES } from "../constants/";
 import styles from "./App.module.css";
 import { inject } from "mobx-react";
@@ -7,7 +7,7 @@ import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
 
 import Game from "./Game";
-import leaderboard from "./Leaderboard";
+import Leaderboard from "./Leaderboard";
 
 const App = ({ uiStore }) => {
   return (
@@ -20,38 +20,16 @@ const App = ({ uiStore }) => {
       </header>
       <main className={styles.layout}>
         <Switch>
-          <Route path={ROUTES.game} exact strict component={Game} />
-          <Route path={ROUTES.leaderboard} component={leaderboard} />
-          <Route path={ROUTES.login} component={Login} />
-          <Route path={ROUTES.register} component={Register} />
           <Route
             path={ROUTES.landing}
             exact
             strict
-            render={() => (
-              <div className={styles.container}>
-                <p className={styles.label}>Welcome, human.</p>
-                {uiStore.authUser ? (
-                  <Link className={styles.link} to={ROUTES.game}>
-                    Play
-                  </Link>
-                ) : (
-                  <ul className={styles.container}>
-                    <li>
-                      <Link className={styles.link} to={ROUTES.login}>
-                        Log in
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className={styles.link} to={ROUTES.register}>
-                        Register
-                      </Link>
-                    </li>
-                  </ul>
-                )}
-              </div>
-            )}
+            component={uiStore.authUser ? Game : Login}
           />
+          <Route path={ROUTES.game} component={Game} />
+          <Route path={ROUTES.leaderboard} component={Leaderboard} />
+          <Route path={ROUTES.login} component={Login} />
+          <Route path={ROUTES.register} component={Register} />
         </Switch>
       </main>
     </>
