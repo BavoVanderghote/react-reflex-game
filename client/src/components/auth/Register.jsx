8fd2ff56476363f59c8b48840d8d5d9f2718da65
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-25 11:29:22 +0200

    links naar registratie en login + styling

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -3,6 +3,7 @@ import { inject } from "mobx-react";
 import { withRouter } from "react-router-dom";
 import { ROUTES } from "../../constants";
 import styles from "./Register.module.css";
+import { Link } from "react-router-dom";
 class Register extends Component {
   constructor() {
     super();
@@ -28,61 +29,66 @@ class Register extends Component {
   render() {
     const { email, pwd, pwd2, name } = this.state;
     return (
-      <div className={styles.container}>
-        <h2 className={styles.label}>first time, human.</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label className={styles.formLabel} htmlFor="email">
-            name
+      <>
+        <div className={styles.container}>
+          <h2 className={styles.label}>first time, human.</h2>
+          <form onSubmit={this.handleSubmit}>
+            <label className={styles.formLabel} htmlFor="email">
+              name
+              <input
+                className={styles.input}
+                type="test"
+                name="name"
+                id="name="
+                value={name}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label className={styles.formLabel} htmlFor="email">
+              email
+              <input
+                className={styles.input}
+                type="email"
+                name="email"
+                id="email="
+                value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label className={styles.formLabel} htmlFor="username">
+              password
+              <input
+                className={styles.input}
+                type="password"
+                name="pwd"
+                id="pwd"
+                value={pwd}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label className={styles.formLabel} htmlFor="username">
+              repeat password
+              <input
+                className={styles.input}
+                type="password"
+                name="pwd2"
+                id="pwd2"
+                ref={pwd2}
+                onChange={this.handleChange}
+              />
+            </label>
             <input
-              className={styles.input}
-              type="test"
-              name="name"
-              id="name="
-              value={name}
-              onChange={this.handleChange}
+              className={styles.button}
+              type="submit"
+              value="register"
+              disabled={pwd && pwd !== pwd2}
             />
-          </label>
-          <label className={styles.formLabel} htmlFor="email">
-            email
-            <input
-              className={styles.input}
-              type="email"
-              name="email"
-              id="email="
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label className={styles.formLabel} htmlFor="username">
-            password
-            <input
-              className={styles.input}
-              type="password"
-              name="pwd"
-              id="pwd"
-              value={pwd}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label className={styles.formLabel} htmlFor="username">
-            repeat password
-            <input
-              className={styles.input}
-              type="password"
-              name="pwd2"
-              id="pwd2"
-              ref={pwd2}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input
-            className={styles.button}
-            type="submit"
-            value="register"
-            disabled={pwd && pwd !== pwd2}
-          />
-        </form>
-      </div>
+          </form>
+        </div>
+        <Link className={styles.redirect} to="/signin">
+          Been here before? Login
+        </Link>
+      </>
     );
   }
 }
