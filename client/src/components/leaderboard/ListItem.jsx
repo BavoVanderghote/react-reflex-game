4724bc9342fc67d9f9fb845bfb892f695176993e
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-04-25 17:28:01 +0200

    redirect bij restarten van een spel

--- client/src/components/leaderboard/ListItem.jsx
+++ client/src/components/leaderboard/ListItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./ListItem.module.css";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { ROUTES } from "../../constants";
 
-const ListItem = ({ score, onDelete, onRematch }) => {
-  // console.log(score);
-
+const ListItem = ({ score, onDelete, onRematch, history }) => {
   return (
     <div className={styles.container}>
       <div className={styles.winner}>
@@ -18,7 +18,13 @@ const ListItem = ({ score, onDelete, onRematch }) => {
       <button className={styles.delete} onClick={() => onDelete(score)}>
         Delete
       </button>
-      <button className={styles.rematch} onClick={() => onRematch(score)}>
+      <button
+        className={styles.rematch}
+        onClick={() => {
+          onRematch(score);
+          history.push(ROUTES.game);
+        }}
+      >
         Rematch
       </button>
     </div>
@@ -31,4 +37,4 @@ ListItem.propTypes = {
   onRematch: PropTypes.func
 };
 
-export default ListItem;
+export default withRouter(ListItem);
