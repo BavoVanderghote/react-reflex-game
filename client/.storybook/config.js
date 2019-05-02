cb7feb0e167acd350d48d9c31204eb993a52941c
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-05-02 16:54:20 +0200

    storybook finaliseren

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,9 @@
-import { configure } from "@storybook/react";
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
 import "../src/index.css";
+import { MemoryRouter } from "react-router-dom";
+
+addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>);
 
 const req = require.context("../src", true, /.stories.js$/);
 
