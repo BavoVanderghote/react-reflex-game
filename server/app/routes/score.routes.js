0b6782a022d32ba1c0c33ade777ba69038d31e42
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-05-02 21:46:40 +0200

    aanpassingen voor deploy

--- server/app/routes/score.routes.js
+++ server/app/routes/score.routes.js
@@ -1,9 +1,9 @@
 module.exports = app => {
   const {checkToken} = require('../middleware/');
   const controller = require('../controllers/score.controller.js');
-  app.post('/api/score', checkToken, controller.create);
-  app.get('/api/score', checkToken, controller.findAll);
-  app.get('/api/score/:scoreId', checkToken, controller.findOne);
-  app.put('/api/score/:scoreId', checkToken, controller.update);
-  app.delete('/api/score/:scoreId', checkToken, controller.delete);
+  app.post('/score', checkToken, controller.create);
+  app.get('/score', checkToken, controller.findAll);
+  app.get('/score/:scoreId', checkToken, controller.findOne);
+  app.put('/score/:scoreId', checkToken, controller.update);
+  app.delete('/score/:scoreId', checkToken, controller.delete);
 };
